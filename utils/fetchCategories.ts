604d4a936da841cbddd97a2a17ffabf5a4d3ee01
ksowah <sowahkelvin640@gmail.com>
2022-08-26 23:23:54 +0000

    in progress

---
+++ utils/fetchCategories.ts
@@ -0,0 +1,7 @@
+// export const fetchCategories = async () => {
+//     const {data} = await fetch(`${process.env.NEXT_PUBLIC_BASIC_URL}/api/getCategories`)
+//     console.log(data);
+    
+//     const categories  
+// }
+
