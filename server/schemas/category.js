ce67091be854b6c21bb21562eb1a7ef4a97b093d
ksowah <sowahkelvin640@gmail.com>
2022-08-27 23:53:17 +0000

    build categories schema

--- server/schemas/category.js
+++ server/schemas/category.js
@@ -1,37 +1,24 @@
+import { BiCategory } from "react-icons/bi";
+
 export default {
-  name: 'category',
-  title: 'Category',
-  type: 'document',
+  name: "category",
+  title: "Category",
+  type: "document",
+  icon: BiCategory,
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'title',
+        source: "title",
         maxLength: 96,
       },
     },
-    {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    },
-    {
-      name: 'parents',
-      title: 'Parent categories',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          to: [{type: 'category'}],
-        },
-      ],
-    },
   ],
-}
+};
