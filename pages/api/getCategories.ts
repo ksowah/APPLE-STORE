dc0bc25b6ab18e393723cec805939da87a308f85
ksowah <sowahkelvin640@gmail.com>
2022-08-27 23:52:23 +0000

    build get categories route

--- pages/api/getCategories.ts
+++ pages/api/getCategories.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { groq } from "next-sanity"
+import { sanityClient } from "../../sanity"
+
+
+const query = groq` *[_type == "category"] {
+  _id,
+  ...
+ }` 
 
 type Data = {
   categories: Category[]
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const categories = await sanityClient.fetch(query)
+  console.log(categories)
+  res.status(200).json({categories})
 }
