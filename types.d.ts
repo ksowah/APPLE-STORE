cbf50aceb77cba043a534947f208aee32b4bbe82
ksowah <sowahkelvin640@gmail.com>
2022-08-27 23:51:19 +0000

    add types

--- types.d.ts
+++ types.d.ts
@@ -10,3 +10,31 @@ interface Category {
     }
     title: string
 }
+
+interface Image {
+    _key: string
+    _type: "image"
+    assets: {
+        url: string
+    }
+}
+
+interface Product {
+    _id: string
+    _createdAt: string
+    _updatedAt: string
+    _rev: string
+    type: "product"
+    slug: {
+        _type: "slug"
+        current: string
+    }
+    description: string
+    category: {
+        _ref: string
+        _type: string
+    }
+    image: Image[]
+    price: number
+    title: string
+}
