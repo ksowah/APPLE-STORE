fb50c768c89ff12fbbc4509b7b3b9294aaab9966
ksowah <sowahkelvin640@gmail.com>
2022-08-27 23:52:01 +0000

    fetch and use data from sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,23 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Landing from '../components/Landing'
+import Product from '../components/Product'
+import { fetchCategories } from '../utils/fetchCategories'
+import { fetchProducts } from '../utils/fetchProducts'
 
-const Home: NextPage = () => {
+
+interface Props {
+  categories: Category[]
+  products: Product[]
+}
+
+const Home = ({categories, products}: Props) => {
+
+  const showProducts = (category: number) => {
+    return products.filter(product => product.category._ref === categories[category]._id)
+    .map( product => <Product product={product} key={product._id} /> ) // return products by category
+  }
+
   return (
     <div className="">
       <Head>
@@ -27,14 +42,30 @@ const Home: NextPage = () => {
 
       <Tab.Group>
         <Tab.List className={"flex justify-center"}>
-
+          {categories.map(category => (
+            <Tab 
+            key={category._id}
+            id={category._id}
+            className={({selected}) =>
+            `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6
+            md:text-base ${
+              selected ? 
+              "borderGradient bg-[#35383c] text-white"
+              : "border-b-2 border-[#35383c] text-[#747474]"
+            }
+            `
+          }
+            >
+              {category.title}
+            </Tab>
+          ))}
         </Tab.List>
 
-        <Tab.Panels className={"mx-auto max-w-fi pt-10 pb-24 sm:px-4"} >
-          {/* <Tab.Panel className={"tabPanel"} >{showProducts(0)}</Tab.Panel>
+        <Tab.Panels className={"mx-auto max-w-fit pt-10 pb-24 sm:px-4"} >
+          <Tab.Panel className={"tabPanel"} >{showProducts(0)}</Tab.Panel>
           <Tab.Panel className={"tabPanel"} >{showProducts(1)}</Tab.Panel>
           <Tab.Panel className={"tabPanel"} >{showProducts(2)}</Tab.Panel>
-          <Tab.Panel className={"tabPanel"} >{showProducts(3)}</Tab.Panel> */}
+          <Tab.Panel className={"tabPanel"} >{showProducts(3)}</Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
      </div>
@@ -47,10 +78,14 @@ const Home: NextPage = () => {
 export default Home
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  // const categories = await fetchCategories
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const categories = await fetchCategories()
+  const products = await fetchProducts()
 
   return {
-    props: {}
+    props: {
+      categories,
+      products
+    }
   }
 }
