6480225311888fd6ca2782f44d6e193c466777ef
ksowah <sowahkelvin640@gmail.com>
2022-08-28 22:31:19 +0000

    build product

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,5 +1,8 @@
 import { ShoppingCartIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
+import toast from 'react-hot-toast'
+import { useRecoilState } from 'recoil'
+import { cartState } from '../atoms/cartAtom'
 import { urlFor } from '../sanity'
 
 
@@ -8,6 +11,14 @@ interface Props {
 }
 
 const Product = ({product}: Props) => {
+
+    const [cart, setCart] = useRecoilState(cartState)
+
+    const addItemToCart = () => {
+          setCart([...cart].concat(product))
+          toast.success(`${product.title} added to cart `)
+    }
+
   return (
     <div className='flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#35383c] p-8 md:h-[500px] md:w-[400px] md:p-10'>
         <div className='relative h-64 w-full md:h-72' >
@@ -20,7 +31,9 @@ const Product = ({product}: Props) => {
                 <p>{product.price}</p>
             </div>
 
-            <div className='flex h-16 w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 md:h-[70px] md:w-[70px]'>
+            <div className='flex h-16 w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full 
+            bg-gradient-to-r from-pink-500 to-violet-500 md:h-[70px] md:w-[70px]'
+            onClick={addItemToCart}>
                 <ShoppingCartIcon className='h-8 w-8 text-white'/>
             </div>
 
