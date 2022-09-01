ac1a697d12669e9ee129ac236aa6b396a4530c8d
ksowah <sowahkelvin640@gmail.com>
2022-09-01 22:18:37 +0000

    wrap app with session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <RecoilRoot>
-      <Toaster />
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <Toaster />
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </SessionProvider>
   );
 }
 
